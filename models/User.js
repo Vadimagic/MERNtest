d7287469bb684ed5dfbaa55f5159ddbc11ffaa43
Vadim <vadim.prog.2000@gmail.com>
2020-12-25 22:42:39 +0300

    complete register

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const {Schema, model, Types} = require('mongoouse')
+const {Schema, model, Types} = require('mongoose')
 
 const schema = new Schema({
 	email: {type: String, required: true, unique: true},
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,24 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 const router = Router()
 
 // /api/auth
 // /register
 router.post('/register', async (request, response) => {
-	try {
-		const {email, password} = req.body
-	} catch (e) {
-		response.status(500).json({message: 'Error, please try again!'})	
-	}
+	// try {
+	// 	const {email, password} = request.body
+	// 	const candidate = await User.findOne({email})
+	// 	if (candidate) {
+	// 		return response.status(400).json({message: 'Such user already exists'})
+	// 	}
+	// 	const hashedPassword = await bcrypt.hash(password, 12)
+	// 	const user = new User({email, password: hashedPassword})
+	// 	await user.save()
+	// 	response.status(201).json({message: 'User created'})
+	// } catch (e) {
+	// 	response.status(500).json({message: 'Error, please try again!'})	
+	// }
 })
 // /login
 router.post('/login', async (request, response) => {
