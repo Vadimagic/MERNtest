5b7b559ae8ff0faeee965c735e6a8e476e694002
Vadim <vadim.prog.2000@gmail.com>
2020-12-25 22:23:10 +0300

    create routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,18 @@
+const {Router} = require('express')
+const router = Router()
+
+// /api/auth
+// /register
+router.post('/register', async (request, response) => {
+	try {
+		const {email, password} = req.body
+	} catch (e) {
+		response.status(500).json({message: 'Error, please try again!'})	
+	}
+})
+// /login
+router.post('/login', async (request, response) => {
+
+})
+
+module.exports = router
