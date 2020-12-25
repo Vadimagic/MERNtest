739504195cb52f6564f4ef75583771ec17a66438
Vadim <vadim.prog.2000@gmail.com>
2020-12-25 22:50:12 +0300

    fix and add checking registr

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,24 +1,36 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
 
 // /api/auth
 // /register
-router.post('/register', async (request, response) => {
-	// try {
-	// 	const {email, password} = request.body
-	// 	const candidate = await User.findOne({email})
-	// 	if (candidate) {
-	// 		return response.status(400).json({message: 'Such user already exists'})
-	// 	}
-	// 	const hashedPassword = await bcrypt.hash(password, 12)
-	// 	const user = new User({email, password: hashedPassword})
-	// 	await user.save()
-	// 	response.status(201).json({message: 'User created'})
-	// } catch (e) {
-	// 	response.status(500).json({message: 'Error, please try again!'})	
-	// }
+router.post('/register', [
+		check('email', 'Incorrect email').isEmail(),
+		check('password', 'Min length 6 symbols').isLength({min: 6})
+	], async (request, response) => {
+	try {
+		const errors = validationResult(request)
+		if (errors.isEmpty()) {
+			return response.status(400).json({
+				errors: errors.array(),
+				message: 'Incorrect data at registration'
+			})
+		}
+
+		const {email, password} = request.body
+		const candidate = await User.findOne({email})
+		if (candidate) {
+			return response.status(400).json({message: 'Such user already exists'})
+		}
+		const hashedPassword = await bcrypt.hash(password, 12)
+		const user = new User({email, password: hashedPassword})
+		await user.save()
+		response.status(201).json({message: 'User created'})
+	} catch (e) {
+		response.status(500).json({message: 'Error, please try again!'})	
+	}
 })
 // /login
 router.post('/login', async (request, response) => {
