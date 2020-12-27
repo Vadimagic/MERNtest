c596c592d9442b827e13ef41f3eee3addc9338bf
Vadim <vadim.prog.2000@gmail.com>
2020-12-27 23:57:48 +0300

    create card in auth page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,7 +1,22 @@
 export const Auth = () => {
 	return (
-		<div>
-			<h1>Auth</h1>
+		<div className="row">
+			<div className="col s6 offset-s3">
+				<h1>Сократи ссылку</h1>
+				<div class="card">
+					<div class="card-image">
+						<img src="https://www.mirf.ru/wp-content/uploads/2020/01/Deca-Dense.jpg"/>
+						<span class="card-title amber-text text-darken-4"><strong>Авторизация</strong></span>
+					</div>
+					<div class="card-content">
+						<p>I am a very simple card. I am good at containing small bits of information.
+						I am convenient because I require little markup to use effectively.</p>
+					</div>
+					<div class="card-action">
+						<a href="#">This is a link</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
