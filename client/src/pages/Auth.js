5ec34a2b6da10f026f5ce4873be282c1768c6f35
Vadim <vadim.prog.2000@gmail.com>
2020-12-28 00:16:19 +0300

    create state & method change state

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,4 +1,15 @@
+import { useState } from "react"
+
 export const Auth = () => {
+	const [form, setForm] = useState({
+		email: '',
+		password: ''
+	})
+
+	const changeHandler = event => {
+		setForm({ ...form, [event.target.name]: event.target.value})
+	}
+
 	return (
 		<div className="row">
 			<div className="col s6 offset-s3">
@@ -9,26 +20,28 @@ export const Auth = () => {
 						<span class="card-title amber-text text-darken-4" style={{fontWeight: 'bold'}}>Авторизация</span>
 					</div>
 					<div class="card-content">
-					<div class="input-field">
-						<input 
-							placeholder="Введите Email" 
-							id="email" 
-							type="text"
-							name="email"
-							className="amber-input"
-						/>
-						<label htmlFor="email">Email</label>
-					</div>
-					<div class="input-field">
-						<input 
-							placeholder="Введите пароль" 
-							id="password" 
-							type="text"
-							name="password"
-							className="amber-input"
-						/>
-						<label htmlFor="password">Пароль</label>
-					</div>
+						<div class="input-field">
+							<input 
+								placeholder="Введите Email" 
+								id="email" 
+								type="text"
+								name="email"
+								className="amber-input"
+								onChange={changeHandler}
+							/>
+							<label htmlFor="email">Email</label>
+						</div>
+						<div class="input-field">
+							<input 
+								placeholder="Введите пароль" 
+								id="password" 
+								type="text"
+								name="password"
+								className="amber-input"
+								onChange={changeHandler}
+							/>
+							<label htmlFor="password">Пароль</label>
+						</div>
 					</div>
 					<div class="card-action">
 						<button className="btn amber darken-2" style={{marginRight: 10}}>Войти</button>
