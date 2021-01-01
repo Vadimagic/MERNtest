cc6168b74dd26a9736d28bfd5d4059ae417eefa1
Vadim <vadim.prog.2000@gmail.com>
2021-01-02 00:37:19 +0300

    complete visible detail link

---
+++ client/src/components/LinkCard.js
@@ -0,0 +1,20 @@
+export const LinkCard = ({link}) => {
+	return (
+		<div className="row">
+			<div className="col s6 offset-s3">
+				<div className="card">
+					<div className="card-image">
+						<img src={`https://picsum.photos/600/300`} alt=""/>
+						<span className="card-title">Сокращенная ссылка Title</span>
+					</div>
+					<div className="card-content" style={{overflow: 'hidden'}}>
+						<p>Ваша ссылка: <a href={link.to} target="_blank" rel="noreferrer">{link.to}</a></p>
+						<p>Откуда: <a href={link.from} target="_blank" rel="noreferrer">{link.from}</a></p>
+						<p>Количество кликов по ссылке: <strong>{link.clicks}</strong></p>
+						<p>Дата создания: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
