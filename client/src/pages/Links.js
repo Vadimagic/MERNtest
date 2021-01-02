9441bfc5fed1f1f1c131f99e89303ab0620f8635
Vadim <vadim.prog.2000@gmail.com>
2021-01-02 23:47:43 +0300

    complete inks page

--- client/src/pages/Links.js
+++ client/src/pages/Links.js
@@ -1,7 +1,31 @@
+import { useCallback, useContext, useEffect, useState } from 'react'
+import { useHttp } from '../hooks/http.hook'
+import { AuthContext } from '../context/AuthContext'
+import { Loader } from '../components/Loader'
+
 export const Links = () => {
+	const [links, setLinks] = useState([])
+	const {loading, request} = useHttp()
+	const {token} = useContext(AuthContext)
+
+	const fetchLinks = useCallback(async () => {
+		try {
+			const fetched = await request('/api/link', 'GET', null, { Authorization: `Bearer ${token}`})
+			setLinks(fetched)
+		} catch (e) {}
+	}, [token, request])
+
+	useEffect(() => {
+		fetchLinks()
+	}, [fetchLinks])
+
+	if (loading) {
+		return <Loader />
+	}
+
 	return (
-		<div>
-			<h1>Links</h1>
-		</div>
+		<>
+			{!loading && <LinksList links={links}/>}
+		</>
 	)
 }
